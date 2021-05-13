d418a784bab70170e52914c3f985aede9a21bfed
steffilazerte <steffi@steffi.ca>
2021-05-13 12:32:18 -0500

    Choose dates individually per obs well within a range of the original report dates - Tables indicate which values are based on these dates - Table 4 uses most recent date from CURRENT year

--- func_summarize.R
+++ func_summarize.R
@@ -15,6 +15,8 @@
 well_report <- function(ows, report_dates = Sys.Date(), within = 7,
                         clean_cache = TRUE) {
 
+  report_dates <- dates_check(report_dates)
+
   # Remove cache files not from today
   if(clean_cache) cache_clean()
 
@@ -30,13 +32,11 @@ well_report <- function(ows, report_dates = Sys.Date(), within = 7,
   }
 
   message(glue("- Fetching/cleaning obs well data ({length(ows)} wells)"))
-  w_full <- well_prep(ows, water_year_start = 10)
-  report_dates <- dates_extract(w_full, report_dates, within)
-  wy <- unique(w_full$WaterYear[w_full$Date == max(report_dates)])
-  w_full <- mutate(w_full, CurrentYear = WaterYear == wy)
+  w_full <- well_prep(ows, water_year_start = 10, report_dates)
+  w_dates <- well_dates(w_full, report_dates, within)
   message("- Summarizing historical statistics")
   w_hist <- well_hist(w_full)
-  w_comp <- well_hist_compare(w_full, report_dates)
+  w_comp <- well_hist_compare(w_dates, w_full)
   message("- Summarizing percentiles")
   w_perc <- well_percentiles(w_comp)
 
@@ -44,11 +44,12 @@ well_report <- function(ows, report_dates = Sys.Date(), within = 7,
   render("report.Rmd",
          params = list("w_full" = w_full, "w_hist" = w_hist,
                        "w_comp" = w_hist, "w_perc" = w_perc,
+                       "w_dates" = w_dates,
                        "report_dates" = report_dates),
          output_dir = "reports", output_file = glue("report_{Sys.Date()}.pdf"))
 }
 
-dates_extract <- function(w_full, report_dates, within) {
+dates_check <- function(report_dates) {
 
   report_dates <- suppressWarnings(as_date(report_dates))
   if(any(is.na(report_dates))) {
@@ -57,33 +58,37 @@ dates_extract <- function(w_full, report_dates, within) {
     stop("Cannot calculate reports for future dates", call. = FALSE)
   }
 
-  dates <- dates_closest(w_full, report_dates, within)      # Get closest
-  dates2 <- dates_closest(w_full, dates - years(1), within) # Get closest 1 year ago
-
-  sort(c(dates, dates2), decreasing = TRUE)
+  sort(c(report_dates, report_dates - years(1)), decreasing = TRUE)
 }
 
-dates_closest <- function(w_full, report_dates, within) {
-  dates <- as_date("")
-  for(i in seq_along(report_dates)) {
-    dates[i] <- w_full %>%
-      filter(!is.na(Value),
-             Date >= report_dates[i] - days(within),
-             Date <= report_dates[i] + days(within)) %>%
-      count(Date) %>%
-      filter(n == max(n)) %>%
-      arrange(desc(Date == report_dates[i]), desc(Date)) %>%
-      slice(1) %>%
-      pull(Date)
-  }
-  dates
+well_dates <- function(w_full, report_dates, within) {
+
+  r <- tibble(report_dates = report_dates) %>%
+    mutate(Date = map(report_dates, ~seq(. - days(within),
+                                          . + days(within),
+                                          by = "1 day"))) %>%
+    unnest(Date)
+
+  w_full %>%
+    right_join(r, by = "Date") %>%
+    group_by(ow, report_dates) %>%
+    arrange(abs(Date - report_dates)) %>%
+    mutate(keep = if_else(all(is.na(Value)), Date[1], Date[first(which(!is.na(Value)))])) %>%
+    filter(Date == keep) %>%
+    ungroup() %>%
+    select(-keep)
 }
 
-well_prep <- function(ows, water_year_start) {
-  well_dl(ows) %>%
-    mutate(data = map(data, well_clean, water_year_start = water_year_start)) %>%
+well_prep <- function(ows, water_year_start, report_dates) {
+  w <- well_dl(ows) %>%
+
+    mutate(data = map(data, well_clean, water_year_start = water_year_start, report_dates)) %>%
     unnest(data) %>%
     select(-file)
+
+  wy <- unique(w$WaterYear[w$Date == max(report_dates)])
+
+  mutate(w, CurrentYear = WaterYear == wy)
 }
 
 wy_calc <- function(report_dates, water_year_start) {
@@ -126,7 +131,7 @@ well_dl <- function(ows) {
     mutate(data = map2(file, ow, well_read))
 }
 
-well_clean <- function(w, water_year_start) {
+well_clean <- function(w, water_year_start, report_dates) {
   w %>%
     select(-myLocation) %>%
     mutate(Date = as_date(Time)) %>%
@@ -138,6 +143,7 @@ well_clean <- function(w, water_year_start) {
     group_by(Date, Approval) %>%
     summarise(Value = mean(Value, na.rm = TRUE), .groups = "drop") %>%
     find_continuous() %>%
+    complete(Date = report_dates) %>%
     # Fill in dates with missing values with NA and add various date columns
     fill_missing_dates(water_year_start = water_year_start) %>%
     add_date_variables(water_year_start = water_year_start) %>%
@@ -182,12 +188,10 @@ well_hist <- function(w_full) {
               p = list(ecdf(Value)), .groups = "drop")
 }
 
-well_hist_compare <- function(w_full, report_dates = NULL) {
-  w_full %>%
-    filter(Date %in% report_dates) %>%
+well_hist_compare <- function(w_dates, w_full) {
+  w_dates %>%
     left_join(well_hist(w_full), by = c("ow", "DayofYear")) %>%
-    mutate(percentile = map2_dbl(p, Value, ~.x(.y))) %>%
-    ungroup()
+    mutate(percentile = map2_dbl(p, Value, ~.x(.y)))
 }
 
 perc_match <- function(x, cols = "class") {
@@ -197,21 +201,25 @@ perc_match <- function(x, cols = "class") {
 well_percentiles <- function(w_comp) {
   w_comp %>%
     well_meta() %>%
-    select(ow, Date, percentile, type) %>%
+    select(ow, Date, report_dates, percentile, type) %>%
     filter(!is.na(percentile)) %>%
     mutate(class = map_chr(percentile, perc_match, cols = "nice")) %>%
     select(-percentile) %>%
-    group_by(Date, class, type) %>%
-    summarize(n_class_type = n(), .groups = "drop") %>%
-    complete(Date = unique(w_comp$Date), type = unique(type_values$type),
+    mutate(window = Date != report_dates) %>%
+    group_by(report_dates, class, type) %>%
+    summarize(n_class_type = n(),
+              window = any(window),
+              .groups = "drop") %>%
+    complete(report_dates = unique(w_comp$report_dates), type = unique(type_values$type),
              class = perc_values$nice, fill = list(n_class_type = 0)) %>%
-    group_by(Date, type) %>%
+    group_by(report_dates, type) %>%
     mutate(n_total_type = sum(n_class_type)) %>%
-    group_by(Date, class) %>%
+    group_by(report_dates, class) %>%
     mutate(n_total_class = sum(n_class_type)) %>%
     ungroup() %>%
-    group_by(Date) %>%
-    mutate(n_total_date = sum(n_class_type)) %>%
+    group_by(report_dates) %>%
+    mutate(n_total_date = sum(n_class_type),
+           window = any(window, na.rm = TRUE)) %>%
     ungroup()
 }
 
