dd221a64dde5f62b6963422928c1603f7addd2e4
steffilazerte <steffi@steffi.ca>
2021-11-17 14:49:16 -0600

    Change colours to original scale

--- data-raw/internal.R
+++ data-raw/internal.R
@@ -18,17 +18,18 @@ data_types <- c("wells_sf", "aquifers", "regions")
 #scales::show_col(viridis::viridis(n = 7, option = "B", end = 0.8), ncol = 1) # Okay
 #scales::show_col(viridis::viridis(n = 7, option = "C"), ncol = 1) # --> No good
 #scales::show_col(viridis::viridis(n = 7, option = "D"), ncol = 1) # Use this one
+#perc_colours <- viridis::viridis(n = 7, option = "D")
 
-perc_colours <- viridis::viridis(n = 7, option = "D")
+perc_colours <- c("black", "blue", "cyan", "green", "#FFCC00", "#800000", "red")
 
 perc_values <- dplyr::tribble(
   ~class,      ~nice,               ~txt_colour, ~low,    ~high,   ~low_show, ~high_show,
   "p_max",     "Maximum",           "white",     0,       0.00001, 1,         1,
   "p_v_high",  "Much Above Normal", "white",     0.00001, 0.10,    0.9,       1,
-  "p_m_high",  "Above Normal",      "white",     0.10,    0.25,    0.75,      0.9,
+  "p_m_high",  "Above Normal",      "black",     0.10,    0.25,    0.75,      0.9,
   "p_n",       "Normal",            "black",     0.25,    0.75,    0.25,      0.75,
   "p_m_low",   "Below Normal",      "black",     0.75,    0.9,     0.10,      0.25,
-  "p_v_low",   "Much Below Normal", "black",     0.9,     0.99999, 0,         0.10,
+  "p_v_low",   "Much Below Normal", "white",     0.9,     0.99999, 0,         0.10,
   "p_min",     "Minimum",           "black",     0.99999, 1,       0,         0) %>%
   dplyr::mutate(colour = !!perc_colours)
 
